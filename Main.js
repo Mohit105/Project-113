4d75a9bb87a20c3e24de3b714d2a2dabdfd8462d
Mohit105 <88244415+Mohit105@users.noreply.github.com>
2021-10-11 11:10:14 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,39 @@
+function preload()
+{
+    img = loadImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7wq6J2UNVAMclOMBtcJjkV3ZJwKSkzv0l24ailNVqBMAuUWGtXgZn_go50EIKkFcDi8g&usqp=CAU')
+}
+
+function setup() 
+{
+    canvas = createCanvas(500,400);
+    canvas.position(70,200);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() 
+{
+    image(video,100,80,300,250);
+
+    stroke(168, 10, 10);
+    fill(168, 10, 10);
+    
+    circle(40,40,50);
+    circle(460,40,50);
+    circle(40,360,50);
+    circle(460,360,50);
+
+
+    stroke(50, 168, 82);
+    fill(50, 168, 82);
+
+    rect(63, 27, 375, 25);
+    rect(63, 347, 375, 25);
+    rect(27, 63, 25, 275);
+    rect(448, 63, 25, 275);
+}
+
+function take_snapshot()
+{
+    save("B-day pic.png");
+}
